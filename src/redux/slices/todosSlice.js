d88606f464bf84fb36d4fb34dae7f363587fd593
Larysa <lorasperling@gmail.com>
2026-04-14 20:02:59 +0200

    feat: add clear completed functionality

--- src/redux/slices/todosSlice.js
+++ src/redux/slices/todosSlice.js
@@ -4,7 +4,7 @@ const loadTodos = () => {
   try {
     const savedTodos = localStorage.getItem('todos');
     return savedTodos ? JSON.parse(savedTodos) : [];
-  } catch  {
+  } catch {
     return [];
   }
 };
@@ -27,8 +27,26 @@ const todosSlice = createSlice({
     deleteTodo: (state, action) => {
       return state.filter((item) => item.id !== action.payload);
     },
+    clearCompleted: (state) => {
+      return state.filter((todo) => !todo.completed);
+    },
+    editTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.find((item) => item.id === id);
+
+      if (todo && text.trim()) {
+        todo.text = text.trim();
+      }
+    },
   },
 });
 
-export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+export const {
+  addTodo,
+  toggleTodo,
+  deleteTodo,
+  clearCompleted,
+  editTodo,
+} = todosSlice.actions;
+
 export default todosSlice.reducer;
